3655d5060568c28025db50434cc24b4548272616
glance6476 <glance6476@gmail.com>
2020-11-15 15:28:03 -0500

    ran Holland provenance plot

---
+++ QFL.R
@@ -0,0 +1,14 @@
+## load QFL_Yezi.csv as sandstone
+
+## run Holland provenancePlot
+
+provenancePlot(sandstone, 'QFL', pch = 16)
+
+provenancePlot(sandstone, 'QmFLt', pch = 16)
+
+provenancePlot(sandstone, 'QpLvLs', pch = 16)
+
+provenancePlot(sandstone, 'QmPK', pch = 16)
+
+provenancePlot(sandstone, 'allFour', pch = 16)
+
